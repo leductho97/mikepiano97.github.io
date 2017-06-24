576dac70ec5ecc772bb5bef216e8514b79f65454
Le Duc Tho <thold.marketing@gmail.com>
2017-06-25 06:32:36 +0700

    UPload tat ca len may

---
+++ app/tutorial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-tutorial",
+    template: "<h3>Huong dan hoc Angular 2!</h3>"
+})
+
+export class TutorialComponent {
+
+}
